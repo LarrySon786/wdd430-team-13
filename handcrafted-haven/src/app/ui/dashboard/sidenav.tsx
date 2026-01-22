f380c239965aefab76ddf5a928ebe356db1d6eb9
LarrySon786 <richardsbrandon3@gmail.com>
2026-01-22 14:57:39 -0500

    Begining Project

---
+++ handcrafted-haven/src/app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Logo from '@/src/app/ui/dashboard/logo';
+
+
+
+export default function SideNav() {
+    <Link>
+        <Logo />
+    </Link>
+    
+    <NavLinks />
+}
+
