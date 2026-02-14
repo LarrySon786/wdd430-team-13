8583f61f02d207e6121cfa89b827488d6f617581
edemfrancis <efrancis.edem@gmail.com>
2026-02-14 10:32:32 +0100

    Have not change anything, just want to make sure this branch is set

--- handcrafted-haven/src/database/query.tsx
+++ handcrafted-haven/src/database/query.tsx
@@ -1,7 +1,7 @@
-import postgres from 'postgres';
-import bcrypt from 'bcrypt';
+import postgres from "postgres";
+import bcrypt from "bcrypt";
 
-const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
+const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
 
 //Database Tables
 
@@ -14,56 +14,62 @@ const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 // Reviews
 // userId (foreign key), productId (foreign key), Score, message
 
-
 // TEAM TO DO LIST
 //  1. Access to NEON POSTGRES database management?
 //  ALSO share the .env connection string.
 //  2. Review current database tables, columns, and data
 //  3. Difficulties deploying to render. Errors in finding correct folder 404.
 
-
 // UTILITY
 async function hash(password: string) {
-    if (typeof password !== 'string' || password.length === 0) {
-        throw new Error('Password must be a non-empty string.');
-    }
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
+	if (typeof password !== "string" || password.length === 0) {
+		throw new Error("Password must be a non-empty string.");
+	}
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
 
-    return hash;
+	return hash;
 }
 
 // USER table
 async function selectUsers() {
-    const data = await sql`
+	const data = await sql`
     SELECT *
-    FROM users;`
-    console.log(data);
+    FROM users;`;
+	console.log(data);
 }
 
-async function insertUser(firstName: string, lastName: string, email: string, password: string, accountType: string) {
+async function insertUser(
+	firstName: string,
+	lastName: string,
+	email: string,
+	password: string,
+	accountType: string,
+) {
+	const hashedPassword = await hash(password);
 
-    const hashedPassword = await hash(password);
-
-    await sql`
+	await sql`
     INSERT INTO users (firstname, lastname, email, password, accounttype)
     VALUES (${firstName}, ${lastName}, ${email}, ${hashedPassword}, ${accountType})
 `;
 }
 
-
-
-
 // PRODUCT table
 async function selectProducts() {
-    const data = await sql`
+	const data = await sql`
     SELECT *
-    FROM products;`
-    console.log(data);
+    FROM products;`;
+	console.log(data);
 }
 
-async function insertProduct(name: string, description: string, price: number, imageUrl: string, userId: number,) {
-    await sql`
+async function insertProduct(
+	name: string,
+	description: string,
+	price: number,
+	imageUrl: string,
+	userId: number,
+) {
+	await sql`
     INSERT INTO products (name, description, price, imageurl, userid)
     VALUES (${name}, ${description}, ${price}, ${imageUrl}, ${userId})
 `;
@@ -71,66 +77,50 @@ async function insertProduct(name: string, description: string, price: number, i
 
 // REVIEWS Table
 async function selectReviews() {
-    const data = await sql`
+	const data = await sql`
     SELECT *
-    FROM reviews;`
-    console.log(data);
+    FROM reviews;`;
+	console.log(data);
 }
 
-async function insertReview(score: number, message: string, userId: number, productId: number) {
-    await sql`
+async function insertReview(
+	score: number,
+	message: string,
+	userId: number,
+	productId: number,
+) {
+	await sql`
     INSERT INTO reviews (score, message, userid, productid)
     VALUES (${score}, ${message}, ${userId}, ${productId})
-    `
+    `;
 }
-
-
 
 export default async function seedDatabase() {
-
-    // BELOW IS WORKING (Commented out to prevent adding excesive items to database)
-    // USERS
-
-    // try {
-    //     await insertUser('Jose', 'Sanchez', 'testride@flying.com', 'evenfakerpassword', 'user')
-    //     await selectUsers();
-    //     console.log('Database seeding completed')
-    // } catch (error) {
-    //     console.log(error);
-    // }
-
-
-
-
-
-    // BELOW IS WORKING (Commented out to prevent adding excesive items to database)
-    // PRODUCTS
-
-    // try {
-    //     await insertProduct('Cheese', 'Fresh off the farm', 3.00, '/none', 1)
-    //     await selectProducts();
-    //     console.log('Database seeding completed')
-    // } catch (error) {
-    //     console.log(error);
-    // }    
-
-    
-    
-    
-    
-    
-    // BELOW IS WORKING (Commented out to prevent adding excesive items to database)
-    // REVIEWS
-    
-    // try {
-    //     await insertReview(4, "This product is almost perfect", 1, 1)
-    //     await selectReviews();
-    //     console.log('Database seeding completed')
-    // } catch (error) {
-    //     console.log(error);
-    // }   
+	// BELOW IS WORKING (Commented out to prevent adding excesive items to database)
+	// USERS
+	// try {
+	//     await insertUser('Jose', 'Sanchez', 'testride@flying.com', 'evenfakerpassword', 'user')
+	//     await selectUsers();
+	//     console.log('Database seeding completed')
+	// } catch (error) {
+	//     console.log(error);
+	// }
+	// BELOW IS WORKING (Commented out to prevent adding excesive items to database)
+	// PRODUCTS
+	// try {
+	//     await insertProduct('Cheese', 'Fresh off the farm', 3.00, '/none', 1)
+	//     await selectProducts();
+	//     console.log('Database seeding completed')
+	// } catch (error) {
+	//     console.log(error);
+	// }
+	// BELOW IS WORKING (Commented out to prevent adding excesive items to database)
+	// REVIEWS
+	// try {
+	//     await insertReview(4, "This product is almost perfect", 1, 1)
+	//     await selectReviews();
+	//     console.log('Database seeding completed')
+	// } catch (error) {
+	//     console.log(error);
+	// }
 }
-
-
-
-
